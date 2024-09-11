920f7bf2f663b026f2814760af4f55e0ee37357a
Mykola322 <mikolasutij2009@gmail.com>
2024-09-11 21:28:24 +0300

    телеграм бот

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from aiogram.types import Message
 from aiogram.utils.markdown import hbold
 from aiogram.client.default import DefaultBotProperties
 
-
+from  app.routers import film_router
 
 
 # Завантажимо дані середовища з файлу .env(За замовчуванням)
@@ -17,12 +17,13 @@ load_dotenv()
 
 # Усі обробники варто закріплювати за Router або Dispatcher
 root_router = Router()
-
+root_router.include_routers(film_router)
 
 # Обробник для команди /start
 @root_router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-   await message.answer(f"Вітаю, {hbold(message.from_user.full_name)}!")
+   await message.answer(f"Вітаю, {hbold(message.from_user.full_name)}!\
+                                                                                                      Введіть команду Films щоб обрати фільм")
 
 
 
@@ -30,7 +31,7 @@ async def command_start_handler(message: Message) -> None:
 # Головна функція пакету
 async def main() -> None:
    # Дістанемо токен бота з середовища
-   TOKEN = getenv("BOT_APT")
+   TOKEN = getenv("BOT_API")
    # Створимо об'єкт Bot
    bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
